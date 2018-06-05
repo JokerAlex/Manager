0bcabdfc91b7e59bcd7d7a7c564896bbed023598
JokerAlex <alex.zhao1023@gmail.com>
2018-06-05 22:13:03 +0800

    项目初始化

---
+++ src/main/java/com/manager/main/App.java
@@ -0,0 +1,21 @@
+package com.manager.main;
+
+import com.manager.controller.LoginController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //获取上下文对象
+        ApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        LoginController loginController = context.getBean(LoginController.class);
+        loginController.initController();
+    }
+}
