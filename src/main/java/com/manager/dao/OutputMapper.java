629762d6a279b056419536ea1c2f2e2f2908ad28
JokerAlex <alex.zhao1023@gmail.com>
2018-06-06 22:41:45 +0800

    产值dao

--- src/main/java/com/manager/dao/OutputMapper.java
+++ src/main/java/com/manager/dao/OutputMapper.java
@@ -1,4 +1,17 @@
 package com.manager.dao;
 
+import com.manager.pojo.Output;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface OutputMapper {
+
+    List<Output> getAll(@Param("year") int year, @Param("month") int month);
+
+    int insertOne(Output output);
+
+    int changeOutput(Output output);
+
+    int updateOutput(Output output);
 }
