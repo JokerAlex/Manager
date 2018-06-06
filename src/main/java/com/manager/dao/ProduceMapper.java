bd2cb5b16447903b4abbf17a2e4a2f816c03de9f
JokerAlex <alex.zhao1023@gmail.com>
2018-06-06 22:40:39 +0800

    生产进度dao

--- src/main/java/com/manager/dao/ProduceMapper.java
+++ src/main/java/com/manager/dao/ProduceMapper.java
@@ -1,4 +1,19 @@
 package com.manager.dao;
 
+import com.manager.pojo.Produce;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface ProduceMapper {
+
+    List<Produce> getAll(@Param("year") int year, @Param("month") int month, @Param("day") int day);
+
+    int insertOne(Produce produce);
+
+    int insertBatch(List<Produce> produceList);
+
+    int updateOne(Produce produce);
+
+    int changeProduce(Produce produce);
 }
