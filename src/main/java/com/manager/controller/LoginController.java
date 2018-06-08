0edfedfbc4cfa8f63848398195f51661e37bec5a
JokerAlex <alex.zhao1023@gmail.com>
2018-06-09 01:26:55 +0800

    修正

--- src/main/java/com/manager/controller/LoginController.java
+++ src/main/java/com/manager/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.manager.pojo.User;
 import com.manager.service.LoginService;
 import com.manager.views.LoginView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import javax.swing.*;
@@ -14,14 +15,16 @@ public class LoginController {
 
     private LoginView loginView;
     private LoginService loginService;
+    private ApplicationContext context;
 
+    @Autowired
     public LoginController(LoginView loginView, LoginService loginService) {
         this.loginView = loginView;
         this.loginService = loginService;
         initView();
     }
 
-    public void initView() {
+    private void initView() {
         loginView.init();
     }
 
@@ -34,12 +37,16 @@ public class LoginController {
         String password = new String(loginView.getPasswordField().getPassword());
         if (userName == null || userName.equals("")) {
             JOptionPane.showMessageDialog(null, "用户名不能为空", "提示", JOptionPane.INFORMATION_MESSAGE);
-        } else if (password == null || password.equals("")) {
+        } else if (password.equals("")) {
             JOptionPane.showMessageDialog(null, "密码不能为空", "提示", JOptionPane.INFORMATION_MESSAGE);
         } else {
             User user = loginService.checkUser(userName, password);
             if (user != null) {
-                JOptionPane.showMessageDialog(null, "ok", "提示", JOptionPane.INFORMATION_MESSAGE);
+                loginView.setVisible(false);
+                MainController mainController = context.getBean(MainController.class);
+                //初始化页面和事件监听
+                mainController.initview();
+                mainController.inintController();
             } else {
                 JOptionPane.showMessageDialog(null, "用户名或密码错误", "提示", JOptionPane.ERROR_MESSAGE);
             }
@@ -47,5 +54,7 @@ public class LoginController {
 
     }
 
-
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
 }
