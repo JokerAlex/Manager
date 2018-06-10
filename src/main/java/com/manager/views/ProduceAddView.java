a6e90f25e2b35eabdc824449e9a716e832172317
JokerAlex <alex.zhao1023@gmail.com>
2018-06-10 23:08:05 +0800

    增加进度

---
+++ src/main/java/com/manager/views/ProduceAddView.java
@@ -0,0 +1,57 @@
+package com.manager.views;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Component
+public class ProduceAddView extends JFrame{
+    private JLabel chooseLabel;
+    private JComboBox productComboBox;
+    private JButton confirmButton;
+
+    public ProduceAddView() {
+        init();
+    }
+
+    private void init(){
+
+
+        chooseLabel = new JLabel("选择要添加的产品：");
+
+        productComboBox = new JComboBox();
+
+        confirmButton = new JButton("确定");
+
+        JPanel labelPanel = new JPanel();
+        labelPanel.add(chooseLabel);
+
+        JPanel productPanel = new JPanel();
+        productPanel.add(productComboBox);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(confirmButton);
+
+        this.setLayout(new BorderLayout());
+        this.add(labelPanel,BorderLayout.NORTH);
+        this.add(productPanel,BorderLayout.CENTER);
+        this.add(buttonPanel,BorderLayout.SOUTH);
+        this.setTitle("添加新的产品进度");
+        this.setSize(300,150);
+        this.setLocationRelativeTo(null);
+        this.setVisible(false);
+    }
+
+    public JComboBox getProductComboBox() {
+        return productComboBox;
+    }
+
+    public JButton getConfirmButton() {
+        return confirmButton;
+    }
+
+    public static void main(String[] args) {
+        new ProduceAddView();
+    }
+}
