203dbd3535f48442178957c8997cdded86c25fbf
JokerAlex <alex.zhao1023@gmail.com>
2018-06-13 23:23:30 +0800

    新增下单数量

--- src/main/java/com/manager/views/ProduceAddView.java
+++ src/main/java/com/manager/views/ProduceAddView.java
@@ -9,6 +9,8 @@ import java.awt.*;
 public class ProduceAddView extends JFrame{
     private JLabel chooseLabel;
     private JComboBox productComboBox;
+    private JLabel numLabel;
+    private JTextField numTextField;
     private JButton confirmButton;
 
     public ProduceAddView() {
@@ -17,28 +19,38 @@ public class ProduceAddView extends JFrame{
 
     private void init(){
 
+        Dimension dimensionLabel = new Dimension(100,30);
+        Dimension dimensionText = new Dimension(150,30);
 
-        chooseLabel = new JLabel("选择要添加的产品：");
+        chooseLabel = new JLabel("选择产品：");
+        chooseLabel.setPreferredSize(dimensionLabel);
+        numLabel = new JLabel("下单数量：");
+        numLabel.setPreferredSize(dimensionLabel);
 
         productComboBox = new JComboBox();
+        productComboBox.setPreferredSize(dimensionText);
+        numTextField = new JTextField();
+        numTextField.setPreferredSize(dimensionText);
 
         confirmButton = new JButton("确定");
 
-        JPanel labelPanel = new JPanel();
-        labelPanel.add(chooseLabel);
-
         JPanel productPanel = new JPanel();
+        productPanel.add(chooseLabel);
         productPanel.add(productComboBox);
+
+        JPanel numPanel = new JPanel();
+        numPanel.add(numLabel);
+        numPanel.add(numTextField);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(confirmButton);
 
-        this.setLayout(new BorderLayout());
-        this.add(labelPanel,BorderLayout.NORTH);
-        this.add(productPanel,BorderLayout.CENTER);
-        this.add(buttonPanel,BorderLayout.SOUTH);
+        this.setLayout(new FlowLayout());
+        this.add(productPanel);
+        this.add(numPanel);
+        this.add(buttonPanel);
         this.setTitle("添加新的产品进度");
-        this.setSize(300,150);
+        this.setSize(300,200);
         this.setLocationRelativeTo(null);
         this.setVisible(false);
     }
@@ -49,6 +61,10 @@ public class ProduceAddView extends JFrame{
 
     public JButton getConfirmButton() {
         return confirmButton;
+    }
+
+    public JTextField getNumTextField() {
+        return numTextField;
     }
 
     public static void main(String[] args) {
