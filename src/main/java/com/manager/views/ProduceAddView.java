51ec82c9ca6dd390185f846c85fabc0971dd6409
JokerAlex <alex.zhao1023@gmail.com>
2018-06-17 18:40:21 +0800

    该用table显示，添加模糊查询

--- src/main/java/com/manager/views/ProduceAddView.java
+++ src/main/java/com/manager/views/ProduceAddView.java
@@ -3,15 +3,18 @@ package com.manager.views;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 @Component
 public class ProduceAddView extends JFrame{
-    private JLabel chooseLabel;
-    private JComboBox productComboBox;
-    private JLabel numLabel;
+    private JTextField nameTextField;
     private JTextField numTextField;
+    private JButton searchButton;
     private JButton confirmButton;
+
+    private JTable productTable;
+    private DefaultTableModel productTableModel;
 
     public ProduceAddView() {
         init();
@@ -19,24 +22,42 @@ public class ProduceAddView extends JFrame{
 
     private void init(){
 
-        Dimension dimensionLabel = new Dimension(100,30);
-        Dimension dimensionText = new Dimension(150,30);
+        Dimension dimensionLabel = new Dimension(80,30);
+        Dimension dimensionText = new Dimension(100,30);
 
-        chooseLabel = new JLabel("选择产品：");
-        chooseLabel.setPreferredSize(dimensionLabel);
-        numLabel = new JLabel("下单数量：");
+        JLabel nameLabel = new JLabel("产品查询");
+        nameLabel.setPreferredSize(dimensionLabel);
+        nameTextField = new JTextField();
+        nameTextField.setPreferredSize(dimensionText);
+
+        JLabel numLabel = new JLabel("下单数量：");
         numLabel.setPreferredSize(dimensionLabel);
 
-        productComboBox = new JComboBox();
-        productComboBox.setPreferredSize(dimensionText);
+        productTableModel = new DefaultTableModel(){
+            public boolean isCellEditable(int row, int column)
+            {
+                //不可以编辑
+                return false;
+            }
+        };
+
+        productTable = new JTable(productTableModel);
+        productTable.setShowHorizontalLines(true);
+        productTable.setShowVerticalLines(true);
+        productTable.setFillsViewportHeight(true);
+
         numTextField = new JTextField();
         numTextField.setPreferredSize(dimensionText);
 
+        searchButton = new JButton("查询");
         confirmButton = new JButton("确定");
 
-        JPanel productPanel = new JPanel();
-        productPanel.add(chooseLabel);
-        productPanel.add(productComboBox);
+        JPanel namePanel = new JPanel();
+        namePanel.add(nameLabel);
+        namePanel.add(nameTextField);
+        namePanel.add(searchButton);
+
+        JScrollPane scrollPane = new JScrollPane(productTable);
 
         JPanel numPanel = new JPanel();
         numPanel.add(numLabel);
@@ -45,18 +66,34 @@ public class ProduceAddView extends JFrame{
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(confirmButton);
 
-        this.setLayout(new FlowLayout());
-        this.add(productPanel);
-        this.add(numPanel);
-        this.add(buttonPanel);
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(numPanel,BorderLayout.CENTER);
+        panel.add(buttonPanel,BorderLayout.SOUTH);
+
+        this.setLayout(new BorderLayout());
+        this.add(namePanel,BorderLayout.NORTH);
+        this.add(scrollPane,BorderLayout.CENTER);
+        this.add(panel,BorderLayout.SOUTH);
         this.setTitle("添加新的产品进度");
-        this.setSize(300,200);
+        this.setSize(300,400);
         this.setLocationRelativeTo(null);
         this.setVisible(false);
     }
 
-    public JComboBox getProductComboBox() {
-        return productComboBox;
+    public JTextField getNameTextField() {
+        return nameTextField;
+    }
+
+    public JTable getProductTable() {
+        return productTable;
+    }
+
+    public DefaultTableModel getProductTableModel() {
+        return productTableModel;
+    }
+
+    public JButton getSearchButton() {
+        return searchButton;
     }
 
     public JButton getConfirmButton() {
